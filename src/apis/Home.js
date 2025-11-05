053f882911ae1e0e3cbafc7170c5153acb9cbc7d
qrhqrh <1585634135@qq.com>
2025-11-05 20:47:51 +0800

    home-新鲜好物业实现

--- src/apis/Home.js
+++ src/apis/Home.js
@@ -5,3 +5,28 @@ export function getBannerAPI(){
 
     })
 }
+
+
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url:'/home/new'
+  })
+}
+
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return  httpInstance({
+    url:'/home/hot'
+  })
+}
