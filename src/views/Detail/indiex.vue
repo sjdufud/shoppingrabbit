9efd10868053389f46348274606230ca84ae9fb1
qrhqrh <1585634135@qq.com>
2025-11-08 01:11:32 +0800

    day4-09

--- src/views/Detail/indiex.vue
+++ src/views/Detail/indiex.vue
@@ -2,6 +2,7 @@
 import { getDetail } from '@/apis/detial';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import DetailHot from '@/views/Detail/components/DetailHot.vue';
 const route =useRoute()
 const goods =ref({})
 const getGoods =async()=>{
@@ -120,7 +121,10 @@ onMounted(()=>getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+                <!-- 24小时 -->
+               <DetailHot/>
+                <!-- 周 -->
+               <DetailHot/>
             </div>
           </div>
         </div>
