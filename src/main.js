d0aa302ab23fc81104fefcfed98760bee7a362ad
qrhqrh <1585634135@qq.com>
2025-11-10 15:36:05 +0800

    登录-数据持久化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 //引入初始化样式文件
@@ -17,7 +17,9 @@ import { lazyPlugin } from '@/directives/index'
 import {componentPlugin}from "@/components"
 
 const app = createApp(App)
-app.use(createPinia())
+const pinia =createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
--- src/stores/user.js
+++ src/stores/user.js
@@ -17,6 +17,8 @@ export const useUserStore=defineStore('user',()=>{
         userInfo,
         getUserInfo
     }
-
-
+},
+{
+    persist:true
+   
 })
