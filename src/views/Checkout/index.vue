3441997902878529fdd79cc44b1b3d89e4c8cd39
qrhqrh <1585634135@qq.com>
2025-11-12 15:29:44 +0800

    切换地址的激活交互

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import {getCheckInfoAPI} from '@/apis/checkout'
+import { ElMessage } from 'element-plus'
 import { onMounted, ref } from 'vue'
 
 
@@ -21,6 +22,29 @@ onMounted(()=>getCheckInfo())
 
 //控制弹框打开
 const showDialog =ref(false)
+
+//切换地址
+
+const activeAddress =ref({})
+const switchAddress=(item)=>{
+    activeAddress.value =item
+     console.log('选中的地址:', item) 
+}
+
+const comfirm =()=>{
+   if (activeAddress.value && activeAddress.value.id) {
+        curAddress.value = activeAddress.value
+        showDialog.value = false  // 添加这行关闭弹框
+        console.log('确认选择的地址:', curAddress.value)
+    } else {
+        // 可以添加提示，告诉用户需要先选择地址
+        ElMessage('请先选择一个地址')
+    }
+}
+
+
+
+
 
 </script>
 
@@ -126,7 +150,7 @@ const showDialog =ref(false)
 
 <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
     <div class="addressWrapper">
-        <div class="text item" v-for="item in checkInfo.userAddresses"  :key="item.id">
+        <div class="text item "  :class="{active:activeAddress.id==item.id}"   @click="switchAddress(item)" v-for="item in checkInfo.userAddresses"  :key="item.id">
         <ul>
         <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
         <li><span>联系方式：</span>{{ item.contact }}</li>
@@ -136,8 +160,8 @@ const showDialog =ref(false)
     </div>
     <template #footer>
         <span class="dialog-footer">
-        <el-button>取消</el-button>
-        <el-button type="primary">确定</el-button>
+        <el-button @click="showDialog = false">取消</el-button>
+        <el-button type="primary" @click="comfirm" >确定</el-button>
         </span>
     </template>
 </el-dialog>
