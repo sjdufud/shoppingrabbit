7fae8e0b9fbe7e9276f2543857396f92a718d340
qrhqrh <1585634135@qq.com>
2025-11-12 19:42:37 +0800

    支付结果展示

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/Pay/PayBack.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //path和components对用关系的位置
@@ -45,6 +46,10 @@ const router = createRouter({
       {
         path:'pay',
         component:Pay
+      },
+      {
+        path:'paycallback',
+        component:PayBack
       }
     ]
     },
